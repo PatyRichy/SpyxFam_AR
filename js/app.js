ef8c0a30c3858c2fd99cf1a44cad48f73187525f
PatyRichy <105775329+PatyRichy@users.noreply.github.com>
2022-06-02 16:29:27 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/aaa.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/TFTP.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
